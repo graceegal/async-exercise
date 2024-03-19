dae7da8be9d97ab9fee4414e1c8847e77ccbb9ae
Jazz Cheema <jaskamalc@gmail.com>
2024-03-18 21:19:11 -0700

    modified script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 "use strict";
 
 const NUMBERS_API_BASE = 'http://numbersapi.com';
-const gracie_number = 7;
+const gracee_number = 7;
 
 
 /** get trivia fact from Numbers API and log it to the console */
 
 async function showNumberTrivia() {
-  const response = await fetch(`${NUMBERS_API_BASE}/${gracie_number}/trivia?json`);
+  const response = await fetch(`${NUMBERS_API_BASE}/${gracee_number}/trivia?json`);
   const data = await response.json();
   console.log("showNumberTrivia: ", data.text);
 }
@@ -16,14 +16,11 @@ async function showNumberTrivia() {
 /** make 4 requests to Numbers API and returns the first response; displays trivia
  * fact of first response to console */
 
-async function showNumberRace() {
+async function showNumberRace(nums) {
 
-  const n1 = fetch(`${NUMBERS_API_BASE}/1/trivia?json`);
-  const n2 = fetch(`${NUMBERS_API_BASE}/2/trivia?json`);
-  const n3 = fetch(`${NUMBERS_API_BASE}/3/trivia?json`);
-  const n4 = fetch(`${NUMBERS_API_BASE}/4/trivia?json`);
+  let numberResp = nums.map(n => fetch(`${NUMBERS_API_BASE}/${n}/trivia?json`));
 
-  const answerPromise = await Promise.race([n1, n2, n3, n4]);
+  const answerPromise = await Promise.race(numberResp);
 
   // const response = await fetch((await answerPromise).url);
 
@@ -36,14 +33,11 @@ async function showNumberRace() {
 /** make 4 requests to Numbers API; shows all successful trivia facts and
  * displays errors separately */
 
-async function showNumberAll() {
+async function showNumberAll(nums) {
 
-  const n1 = fetch(`${NUMBERS_API_BASE}/1/trivia?json`);
-  const n2 = fetch(`${NUMBERS_API_BASE}/2/trivia?json`);
-  const n3 = fetch(`${NUMBERS_API_BASE}/3/trivia?json`);
-  const n4 = fetch(`${NUMBERS_API_BASE}/"WRONG"/trivia?json`);
+  let numberResp = nums.map(n => fetch(`${NUMBERS_API_BASE}/${n}/trivia?json`));
 
-  let results = await Promise.allSettled([n1, n2, n3, n4]);
+  let results = await Promise.allSettled(numberResp);
   // console.log("results", results)
 
   let success = [];
@@ -66,8 +60,8 @@ async function showNumberAll() {
 
 async function main() {
   await showNumberTrivia();
-  await showNumberRace();
-  await showNumberAll();
+  await showNumberRace([1, 2, 3, 4]);
+  await showNumberAll([7, 11, 22, "WRONG"]);
 }
 
 
